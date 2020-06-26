1965c42db999ccff5cd0c27d020c43928b953ac3
hans-peter.weidinger <hans-peter.weidinger@dynatrace.com>
2020-06-26 14:55:07 +0200

    add some simple query

--- CosmosSamples.cs
+++ CosmosSamples.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Azure.Cosmos;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 using System.Security.Cryptography.X509Certificates;
@@ -27,10 +29,16 @@ namespace CosmosDemo {
 			var client = CosmosDBProvider.Client;
 
 			var container = client.GetContainer("CoderDojoDemo", "CoderDojoDemo");
-			var iterator = container.GetItemQueryIterator<dynamic>("select * from c where c.coder > 25");
+			var iterator = container.GetItemQueryIterator<dynamic>("select * from c where c.coder < 7");
 			var docs = await iterator.ReadNextAsync();
 			foreach (var doc in docs) {
-				await Console.Out.WriteLineAsync($"{doc.coder,3} {doc.dojo.date,12} {doc.dojo.location}");
+				try {
+					string[] names = ((JArray)doc.names).Select(p => p.ToString()).ToArray();
+					await Console.Out.WriteLineAsync($"{doc.coder,3} {doc.dojo.date,12} {doc.dojo.location,10} {String.Join(",", names)}");
+				}
+				catch (Exception ex) {
+					await Console.Out.WriteLineAsync(ex.Message);
+				}
 			}
 
 		}
@@ -39,6 +47,10 @@ namespace CosmosDemo {
 			var rnd = new Random(DateTime.Now.Millisecond);
 			var client = CosmosDBProvider.Client;
 
+			var codernames = new string[] {
+				"klaus", "claudia", "fritz", "lea", "marvin", "ObiWan", "Joda", "Luke"
+			};
+
 			var container = client.GetContainer("CoderDojoDemo", "CoderDojoDemo");
 			foreach (var ort in new string[] { "Linz", "Wien", "Steyr" }) {
 				DateTime t = new DateTime(2020, 6, 26);
@@ -46,13 +58,21 @@ namespace CosmosDemo {
 				
 				for (int i = 0; i < 10; i++) {
 					t.AddDays(-7);
+					int codersnum = rnd.Next(5, 50);
+
+					List<string> coders = new List<string>();
+					for (int j = 0; j < codersnum; j++) {
+						coders.Add(codernames[rnd.Next(0, codernames.Length - 1)]);
+					};
+
 					dynamic data = new {
 						id = Guid.NewGuid(),
 						dojo = new {
 							date = t.ToShortDateString(),
 							location = ort
 						},
-						coder = rnd.Next(5, 50)
+						coder = rnd.Next(5, 50),
+						names = coders
 					};
 					await container.CreateItemAsync<dynamic>(item: data);
 				}
